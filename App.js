47186d29e35a1c9495a130270aee2dad681558b8
netsus <cwjcsk@naver.com>
2021-08-19 23:57:39 +0900

    #0.4 Getting to know Expo
    윈도우 WSL2에서 React Native 아이폰에서 실행하기
    1. 아이폰에서 expo go 다운로드 및 회원 가입
    2. Vscode에서 아래 명령어 실행 - (아이폰을 WIndows PC에 USB로 연결)
    expo login
    expo start --tunnel
    Tunnel URL not Found 에러 발생시 - 아래 명령어 치고 다시 expo start --tunnel하면 주소가 ip가 아니라 문자열 포함된 주소로 바뀌고, 아이폰에서 expo 실행된다.
    npm install @expo/ngrok@^4.1.0
    
    관련 StackOverFlow 링크 : https://stackoverflow.com/questions/61165919/tunnel-url-not-found-falled-back-to-lan-url-while-running-the-expo-start-tun

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>현두야 사랑해!!바바바바 홀리몰리 쏠리</Text>
       <StatusBar style="auto" />
     </View>
   );
