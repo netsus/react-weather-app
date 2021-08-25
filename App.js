2435264fac3574a9bff16c89b14a943e711a529d
netsus <cwjcsk@naver.com>
2021-08-26 00:40:10 +0900

    #1.1 Loading Screen 1. Loading.js   1) export default function Loading을 생성 : 노랑 백그라운드에 글씨 작성 - style 줌
    2. App.js
      1) Loading 함수 import : import Loading from "./Loading";
      2) export default function App 함수에 Loading 태그
        export default function App() {
          return (
            <Loading />
          );
        }

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>Hello</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>Hello</Text>
-      </View>
-    </View>
+    <Loading />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    color: 'blue',
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueView:{
-    flex:2,
-    backgroundColor: "green"
-  }
-});
