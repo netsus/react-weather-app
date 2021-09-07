ae7abb7b1af8a11aa63ecfbefa688d351b34b965
netsus <cwjcsk@naver.com>
2021-09-07 11:40:00 +0100

    #2.4 Titles and Subtitles 1. <Text> 태그로 title과 subtitle 생성 값을 weatherOptions[condition].title, subtitle로 할당
    2. styles값을 여러개 줄 때, {{ ...styles.스타일변수, ...styles.스타일변수2 }}로 줄 수 있음
    --> 앱과 웹에서 다르게 보임. (웹에선 뒤에께 덮어쓰는 느낌)
    
    수정 전체 코드
    <View style={{ ...styles.halfContainer, ...styles.textContainer }} >
        <Text style={styles.title} >{weatherOptions[condition].title}</Text>
        <Text style={styles.subtitle }>{weatherOptions[condition].subtitle}</Text>

--- Weather.js
+++ Weather.js
@@ -7,61 +7,83 @@ import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 const weatherOptions = {
     Haze: {
       iconName: "md-partly-sunny-sharp",
-      gradient: ["#ee9ca7","#ffdde1"]
+      gradient: ["#ee9ca7","#ffdde1"],
+      title:"Haze",
+      subtitle:""
     },
     Thunderstorm: {
       iconName: "md-thunderstorm",
-      gradient: []
+      gradient: ["#1D2B64","#0052D4"],
+      title:"Thunderstorm",
+      subtitle:""
     },
     Drizzle: {
       iconName: "rainy-sharp",
-      gradient: []
+      gradient: ["#9CECFB","#65C7F7","#0052D4"],
+      title:"이슬비",
+      subtitle:"예기치 않게 적당히 내리는 비"
     },
     Rain: {
       iconName: "umbrella-sharp",
-      gradient: []
+      gradient: ["#2c3e50","#3498db"],
+      title:"Rain",
+      subtitle:""
     },
     Snow: {
       iconName: "ios-snow",
-      gradient: []
+      gradient: ["#E0EAFC","#1D2B64"],
+      title:"Snow",
+      subtitle:""
     },
     Atmosphere: {
       iconName: "ios-sunny-sharp",
-      gradient: []
+      gradient: ["#83a4d4","#b6fbff"],
+      title:"Atmosphere",
+      subtitle:""
     },
     Clear: {
       iconName: "ios-sunny-sharp",
-      gradient: []
+      gradient: ["#6DD5FA","#fffc00","#E0EAFC"],
+      title:"Clear",
+      subtitle:""
     },
     Clouds: {
       iconName: "md-cloud",
-      gradient: []
+      gradient: ["#56CCF2","#E0EAFC","#2F80ED"],
+      title:"Clouds",
+      subtitle:""
     },
     Mist: {
       iconName: "water",
-      gradient: []
+      gradient: ["#F8CDDA", "#1D2B64"],
+      title:"Mist",
+      subtitle:""
     },
     Dust: {
       iconName: "apps",
-      gradient: []
+      gradient: ["#304352","#d7d2cc"],
+      title:"Dust",
+      subtitle:""
     }
   };
 
 export default function Weather({ temp, condition }){
+    const weather = "Clouds"
     return (
     <LinearGradient
-    colors={weatherOptions["Haze"].gradient}
+    colors={weatherOptions[condition].gradient}
     style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.halfContainer}>
             <Ionicons 
                 size={96}
-                name={weatherOptions["Haze"].iconName}
+                name={weatherOptions[condition].iconName}
                 color="white" />
             <Text style={styles.temp} >{temp}℃</Text>
         </View>
-        <View style={styles.halfContainer}>
-            
+        <View style={{ ...styles.halfContainer, ...styles.textContainer }} >
+            <Text style={styles.title} >{weatherOptions[condition].title}</Text>
+            <Text style={styles.subtitle }>{weatherOptions[condition].subtitle}</Text>
         </View>
     </LinearGradient>
     );
@@ -88,5 +110,18 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+      color: "white",
+      fontWeight : "300",
+      fontSize: 44,
+      marginBottom: 15
+    },
+    subtitle:{
+      color: "white",
+      fontWeight : "600",
+      fontSize: 24
+    },
+    textContainer: {
     }
-})
+});
